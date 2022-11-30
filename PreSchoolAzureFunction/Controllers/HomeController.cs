d1616fff81ef5ccdeb5e41f6eaa2e631df665b43
gen <regan035@gmail.com>
2022-11-30 00:36:47 -0600

    addImageFunc

--- PreSchoolAzureFunction/Controllers/HomeController.cs
+++ PreSchoolAzureFunction/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using PreSchoolAzureFunction.Models;
 using System.Diagnostics;
@@ -9,10 +11,12 @@ namespace PreSchoolAzureFunction.Controllers
     {
         private readonly ILogger<HomeController> _logger;
         static readonly HttpClient client= new HttpClient();
+        private readonly BlobServiceClient _blobServiceClient;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, BlobServiceClient blobServiceClient)
         {
             _logger = logger;
+            _blobServiceClient = blobServiceClient;
         }
 
         public IActionResult Index()
@@ -23,7 +27,7 @@ namespace PreSchoolAzureFunction.Controllers
         //http://localhost:7268/api/OnCheckUploadToQueue
 
         [HttpPost]
-        public async Task<IActionResult> Index(CheckinRequest checkinRequest)
+        public async Task<IActionResult> Index(CheckinRequest checkinRequest,IFormFile file)
         {
             //checkinRequest.Id = Guid.NewGuid().ToString();
             
@@ -31,6 +35,20 @@ namespace PreSchoolAzureFunction.Controllers
             {
                 HttpResponseMessage response = await client.PostAsync("http://localhost:7212/api/RequestUploadToQueue", content);
                 string returnValue = response.Content.ReadAsStringAsync().Result;
+            }
+            if(file != null)
+            {
+                var fileName = checkinRequest.Id + Path.GetExtension(file.FileName);
+                BlobContainerClient blobContainerClient = _blobServiceClient.GetBlobContainerClient("imagefunction");
+                var blobClient = blobContainerClient.GetBlobClient(fileName);
+
+                var httpHeaders = new BlobHttpHeaders
+                {
+                    ContentType = file.ContentType
+                };
+
+                await blobClient.UploadAsync(file.OpenReadStream(), httpHeaders);
+                return View();
             }
             return RedirectToAction(nameof(Index));
         }
